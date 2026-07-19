e8233ea0b282754211fcfccc259263247b484456
Prince Raj <rajprince56907@gmail.com>
2026-07-19 14:15:01 +0530

    Fix API route prefixes and root-level start script fallback

--- server/src/app.js
+++ server/src/app.js
@@ -11,8 +11,13 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use('/auth', authRoutes);
+
 app.use('/api/room', require('./routes/roomRoutes'));
+app.use('/room', require('./routes/roomRoutes'));
+
 app.use('/api/judge', require('./routes/judgeRoutes'));
+app.use('/judge', require('./routes/judgeRoutes'));
 //Test route
 app.get('/',(req,res)=>{
     res.send("server is running");
